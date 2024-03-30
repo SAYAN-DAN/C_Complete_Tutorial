c3fbf457e3f246551460c08a30a9536395265b47
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-03-30 20:53:04 +0530

    commit

--- hello.c
+++ hello.c
@@ -1,7 +1,12 @@
 # include<stdio.h>
 
 int main() {
-    printf("hello world");
+    printf("hello world \n");
+    printf("hello world \n");
+    printf("hello world \n");
+    printf("hello world \n");
+    int age = 22;
+    printf("age is %d",age);
     return 0;
 }
 
