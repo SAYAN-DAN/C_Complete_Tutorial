77feb379a84e944f287f5e8efe86ba23c62463d4
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-04-19 21:38:02 +0530

    Commit

--- Loops/Question_using_operator/Q8.c
+++ Loops/Question_using_operator/Q8.c
@@ -1,2 +1,17 @@
 //  Print the factorial of a given number ‘n’.
 
+#include <stdio.h>
+int main()
+{
+    int i, n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    int product = 1;
+    for (i = 1; i <= n; i++)
+    {
+        product = product * i;  
+    }
+    printf("The factorial is: %d", product);
+    return 0;
+}
+
---
+++ Loops/Question_using_operator/Q9.c
@@ -0,0 +1,16 @@
+//  Print the factorials of first ‘n’ number
+
+#include <stdio.h>
+int main()
+{
+    int i, n, factorial;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    factorial = 1;
+    for (i = 1; i <= n; i++)
+    {
+        factorial = factorial * i;
+         printf("Factorial of %d : %d\n", i, factorial);
+    }
+    return 0;
+}
