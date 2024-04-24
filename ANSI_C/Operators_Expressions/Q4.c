bf12ac75bf582b8bb56266aec53f619bd305609a
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-04-24 10:11:31 +0530

    Add Commit

---
+++ ANSI_C/Operators_Expressions/Q4.c
@@ -0,0 +1,12 @@
+
+// Further illustration of arithmetic operators.
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d;
+    a = 15;
+    b = 10;
+    c = ++a + b;
+    return 0;
+}
--- Function_Pointers/pointer3.c
+++ Function_Pointers/pointer3.c
@@ -1,9 +1,21 @@
+// #include<stdio.h>
+// int main() {
+//     int a = 5;
+//     int* x = &a;   
+//     printf("%d\n" , *x);  // print the value of a
+//     int** y = &x;
+//     printf("%d\n" , **y);  // print the value of a
+//     return 0;
+// }
+
+
 #include<stdio.h>
 int main() {
-    int a = 5;
-    int* x = &a;   
-    printf("%d\n" , *x);  // print the value of a
-    int** y = &x;
-    printf("%d\n" , **y);  // print the value of a
+    const int i = 10;
+    int *ptr = &i;   
+    *ptr = 20;
+    printf("%d\n", i);
     return 0;
 }
+
+
