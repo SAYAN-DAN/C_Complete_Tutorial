60294115bc9931d5ffbc51399eb75674338243d4
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-05-06 13:09:30 +0530

    Commit

---
+++ Array/Q13.c
@@ -0,0 +1,21 @@
+// Find the Second largest element in the given Array...
+#include<stdio.h>
+#include<limits.h>
+int main(){
+    int arr[5] = {2,56,76,23,12};
+    //  int arr[5] = {-2,-56,-76,-23,-12};
+    int max = INT_MIN;
+    int smax = INT_MIN;
+    for (int i = 0; i <=4; i++){
+        if (max < arr[i] ){
+            max = arr[i];
+        }        
+    }
+    for (int i = 0; i <=4; i++){
+        if ( arr[i] !=max && smax < arr[i] ){
+            smax = arr[i];
+        }        
+    }
+    printf("The second maximum value is: %d", smax);
+    return 0;        
+}
