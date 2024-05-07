b53368d218e6061ce5885b41e6fcb9db676490d9
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-05-07 07:50:06 +0530

    Commit

--- Array/Q13.c
+++ Array/Q13.c
@@ -6,13 +6,23 @@ int main(){
     //  int arr[5] = {-2,-56,-76,-23,-12};
     int max = INT_MIN;
     int smax = INT_MIN;
-    for (int i = 0; i <=4; i++){
+    // for (int i = 0; i <=4; i++){
+    //     if (max < arr[i] ){
+    //         max = arr[i];
+    //     }        
+    // }
+    // for (int i = 0; i <=4; i++){
+    //     if ( arr[i] !=max && smax < arr[i] ){
+    //         smax = arr[i];
+    //     }        
+    // }
+
+     for (int i = 0; i <=4; i++){
         if (max < arr[i] ){
+            smax = max;
             max = arr[i];
-        }        
-    }
-    for (int i = 0; i <=4; i++){
-        if ( arr[i] !=max && smax < arr[i] ){
+        }
+        else if(smax < arr[i]) {
             smax = arr[i];
         }        
     }
