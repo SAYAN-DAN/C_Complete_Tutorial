02b357aa3f149b8a6905605612c3ff3d00ed978c
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-05-12 20:49:53 +0530

    Commit

---
+++ 2D_Array/Q11.c
@@ -0,0 +1,5 @@
+// Write a program to print the row number having the maximum sum in a given matrix...
+#include<stdio.h>
+int main() {
+    
+}
---
+++ 2D_Array/Q13.c
@@ -0,0 +1,25 @@
+// Write a program to print the transpose of the matrix entered by the user...(Leetcode-867)
+
+#include<stdio.h>
+int main() {
+    int r;
+    printf("Enter the number of rows: ");
+    scanf("%d",&r);
+    int c;
+    printf("Enter the number of columns: ");
+    scanf("%d",&c);
+    int arr[r][c];
+    for (int i = 0; i < r ; i++){
+        for (int j = 0; j < c; j++){
+            scanf("%d", &arr[i][j]);
+        }
+        printf("\n");        
+        }
+    for (int i = 0; i < c ; i++){
+        for (int j = 0; j < r; j++){
+            printf("%d", arr[j][i]);
+        }
+        printf("\n");
+        }
+        return 0;    
+}
