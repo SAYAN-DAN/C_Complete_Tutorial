ea7d8073153b0557e8b702f86a9e3ee7318e62e2
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-05-23 22:25:54 +0530

    Commit

---
+++ pattern_printing_questions/Q20.c
@@ -0,0 +1,26 @@
+//     ****
+//    ****
+//   ****
+//  ****
+// ****
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        // Print spaces
+        for (int j = 0; j < n - i - 1; j++) {
+            printf(" ");
+        }
+        // Print stars
+        for (int k = 0; k < 4; k++) {  // Always print 4 stars
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
