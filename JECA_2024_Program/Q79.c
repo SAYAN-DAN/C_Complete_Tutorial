ce333b592280b822bfdb4ee4ea1bc1c54529c2d8
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-05-30 19:07:25 +0530

    Commit

--- JECA_2024_Program/Q79.c
+++ JECA_2024_Program/Q79.c
@@ -13,11 +13,11 @@ int main(){
 
 
 // 2:- It gives us compile time error................
-int main(){
-    while ()
-    {
-        printf("In while loop");
-        printf("After loop");
-    }
-    return 0;    
-}
+// int main(){
+//     while ()
+//     {
+//         printf("In while loop");
+//         printf("After loop");
+//     }
+//     return 0;    
+// }
