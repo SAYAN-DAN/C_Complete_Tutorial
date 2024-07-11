2f6ed7c15b61627eea0c1075067cc74b5fe434b9
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-07-11 06:52:59 +0530

    Changes

---
+++ 50_C_MCQ/Q1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+   printf(5 + "GeeksQuiz");
+   return 0;
+}
+
+
---
+++ 50_C_MCQ/Q3.c
@@ -0,0 +1,17 @@
+
+// #include<stdio.h>
+// int main() {
+//     z=x + 3 + y->f1 + y->f2; 
+// for (i = 0; i < 200; i = i + 2) 
+// { 
+// if (z > i) 
+// { 
+// p = p + x + 3; 
+// q = q + y->f1; 
+// } else 
+// { 
+// p = p + y->f2; 
+// q = q + x + 3; 
+// } 
+// } 
+// }
